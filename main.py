6bc3e4660f11befc20e2485cdd30034f20f35dc5
lwr-song <lawrence.song@student.nisdtx.org>
2025-04-02 14:19:43 -0500

    Freak,

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ MONITOR_WIDTH = MONITOR_INFO.current_w
 WIDTH = 480
 HEIGHT = 360
 window = pygame.display.set_mode([WIDTH, HEIGHT])
+
+image = pygame.image.load("New Piskel.png")
+window.blit(image, (0, 0))
+pygame.display.flip()
+
+input()

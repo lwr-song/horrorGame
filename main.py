fc98062acce366154111305b7904022afc0435ce
lwr-song <lawrence.song@student.nisdtx.org>
2025-04-02 14:14:55 -0500

    We did it guys

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ MONITOR_INFO = pygame.display.Info()
 MONITOR_HEIGHT = MONITOR_INFO.current_h
 MONITOR_WIDTH = MONITOR_INFO.current_w
 
-
+WIDTH = 480
+HEIGHT = 360
+window = pygame.display.set_mode([WIDTH, HEIGHT])

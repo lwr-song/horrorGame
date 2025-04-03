085675110a0a1a205d1b5012ce6eb453705d1394
horrorgamedeveloper <violetaurorabird@gmail.com>
2025-04-03 14:59:01 -0500

    goober WIP....

--- goober.py
+++ goober.py
@@ -1,2 +1,19 @@
 #the grink
 #grython
+import pygame
+pygame.init()
+
+behaviors = ["aggressive", "docile"]
+names = ["grog", "greg heffley"]
+
+class Goober:
+
+    def __init__(self, window, name, size, sprite, behavior, responses):
+        self.name = name
+        self.sprite = pygame.image.load(sprite)
+        self.responses = self.determine_responses()
+        window.blit(self.sprite, (100,200))
+
+    def determine_responses(self):
+        return "aaa"
+

0f11de9de64bc4e6bd9b6137653579952aa6c29e
lwr-song <lawrence.song@student.nisdtx.org>
2025-04-03 14:11:31 -0500

    Freak,

--- main.py
+++ main.py
@@ -29,26 +29,28 @@ def generate_window(width, height, header_size=20):
         header_size = 20
 
     new_window = pygame.Surface((width, height + header_size))
+    new_window.fill((32, 32, 32))
 
     # Header generation
-    header = pygame.Surface((width, header_size))
-    gradations = max(20, int(width / 4))
-    step_size = width / gradations
+    header = pygame.Surface((width - 4, header_size - 4))
+    gradations = max(20, int((width - 4) / 4))
+    step_size = (width - 4) / gradations
 
     # Creating the gradient in the header
     for i in range(gradations):
         x = i * step_size
-        gradient_step = pygame.Surface((step_size + 1, header_size - 4))
+        gradient_step = pygame.Surface((step_size + 1, header_size))
         gradient_step.fill((0, 0, 200 - (100 * i / gradations)))
-        header.blit(gradient_step, (x, 2))
+        header.blit(gradient_step, (x, 0))
 
+    window_body = pygame.Rect(2, header_size, width - 4, height)
 
-
-    new_window.blit(header, (0, 0))
+    pygame.draw.rect(new_window, (196, 196, 196), window_body)
+    new_window.blit(header, (2, 2))
     return new_window
 
-new_window = generate_window(300, 100)
-window.blit(new_window, (0, 0))
+nw = generate_window(300, 100)
+window.blit(nw, (0, 0))
 pygame.display.flip()
 
 input()
